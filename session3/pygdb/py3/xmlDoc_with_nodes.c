415a0dd851f9af62f0a03b25f20c46cedf7c48c1
Liu Jie <liujie5367@sina.com>
2015-12-12 02:32:14 +0800

    add file

---
+++ session3/pygdb/py3/xmlDoc_with_nodes.c
@@ -0,0 +1,17 @@
+
+#include <libxml/xmlmemory.h>
+#include <libxml/parser.h>
+
+int main()
+{
+	xmlDocPtr doc = xmlNewDoc(BAD_CAST"1.0");
+	xmlNodePtr root_node = 
+		xmlNewNode(NULL, BAD_CAST"root");
+
+	xmlDocSetRootElement(doc, root_node);
+
+	xmlFreeDoc(doc);
+	return 0;
+}
+
+
